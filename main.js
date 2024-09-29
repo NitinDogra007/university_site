22974a6834c67675ee811a658362d7f8135e54cf
Nitin Dogra <nitindogra72@outlook.com>
2024-09-29 19:16:32 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var navLinks = document.querySelector('#navLinks');
+
+// Javascript for Toggle Menu
+
+function showMenu() {
+	navLinks.style.right = '0';
+}
+
+function hideMenu() {
+	navLinks.style.right = '-200px';
+}
